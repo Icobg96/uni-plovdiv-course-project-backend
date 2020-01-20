a71fead6cb49fa0f706bffb4d698a07a7e92fa85
Hristo Ispirov <icobg1996@gmil.com>
2020-01-20 23:19:10 +0200

    create project structure

---
+++ social-network/service/src/main/java/com/social/network/Application.java
@@ -0,0 +1,15 @@
+package com.social.network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Created on 4.12.2019 г.
+ * @Author Hristo Ispirov
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
